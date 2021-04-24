20c09efbb20ddf7a6036f0aaa89dcdccf4ff7aad
2019211001000727-xuhuan <245908199@qq.com>
2021-04-24 13:35:26 +0800

    week 8 in Commit

--- src/main/java/com/xuhuan/dao/UserDao.java
+++ src/main/java/com/xuhuan/dao/UserDao.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -13,22 +15,59 @@ public class UserDao implements IUserDao{
 
     @Override
     public boolean saveUser(Connection con, User user) throws SQLException {
-        return false;
+        String sql="insert into usertable values(?,?,?,?,?)";
+        PreparedStatement pstmt= con.prepareStatement(sql);
+        pstmt.setString(1,user.getUsername());
+        pstmt.setString(2,user.getPassword());
+        pstmt.setString(3,user.getEmail());
+        pstmt.setString(4,user.getGender());
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        pstmt.setString(5,formatter.format(user.getBirthdate()));
+        int a=pstmt.executeUpdate();
+        if(a!=0)
+            return true;
+        else
+            return false;
     }
 
     @Override
     public int deleteUser(Connection con, User user) throws SQLException {
-        return 0;
+        String sql="delete from usertable where id=?";
+        PreparedStatement pstmt= con.prepareStatement(sql);
+        pstmt.setInt(1,user.getId());
+        return pstmt.executeUpdate();
     }
 
     @Override
     public int updateUser(Connection con, User user) throws SQLException {
-        return 0;
+        String sql="update usertable set username=?,password=?,email=?,gender=?,birthdate=? where id=?";
+        PreparedStatement pstmt= con.prepareStatement(sql);
+        pstmt.setString(1,user.getUsername());
+        pstmt.setString(2,user.getPassword());
+        pstmt.setString(3,user.getEmail());
+        pstmt.setString(4,user.getGender());
+        pstmt.setDate(5,new java.sql.Date(user.getBirthdate().getTime()));
+        pstmt.setInt(6,user.getId());
+        return pstmt.executeUpdate();
     }
 
     @Override
     public User findById(Connection con, Integer id) throws SQLException {
-        return null;
+        String sql="select * from usertable where id=?";
+        PreparedStatement pstmt= con.prepareStatement(sql);
+        pstmt.setInt(1,id);
+        ResultSet rs= pstmt.executeQuery();
+        User user=null;
+        if(rs.next()){
+            user=new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthdate(rs.getDate("birthdate"));
+        }
+        return user;
     }
 
     @Override
@@ -54,31 +93,126 @@ public class UserDao implements IUserDao{
 
     @Override
     public List<User> findByUsername(Connection con, String username) throws SQLException {
-        return null;
+        String sql="select * from usertable where username=?";
+        PreparedStatement pstmt= con.prepareStatement(sql);
+        pstmt.setString(1,username);
+        ResultSet rs= pstmt.executeQuery();
+        User user=null;
+        List<User> list=new ArrayList<User>();
+        if(rs.next()){
+            user=new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthdate(rs.getDate("birthdate"));
+            list.add(user);
+        }
+        return list;
     }
 
     @Override
     public List<User> findByPassword(Connection con, String password) throws SQLException {
-        return null;
+        String sql="select * from usertable where password=?";
+        PreparedStatement pstmt= con.prepareStatement(sql);
+        pstmt.setString(1,password);
+        ResultSet rs= pstmt.executeQuery();
+        User user=null;
+        List<User> list=new ArrayList<User>();
+        if(rs.next()){
+            user=new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthdate(rs.getDate("birthdate"));
+            list.add(user);
+        }
+        return list;
     }
 
     @Override
     public List<User> findByEmail(Connection con, String email) throws SQLException {
-        return null;
+        String sql="select * from usertable where email=?";
+        PreparedStatement pstmt= con.prepareStatement(sql);
+        pstmt.setString(1,email);
+        ResultSet rs= pstmt.executeQuery();
+        User user=null;
+        List<User> list=new ArrayList<User>();
+        if(rs.next()){
+            user=new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthdate(rs.getDate("birthdate"));
+            list.add(user);
+        }
+        return list;
     }
 
     @Override
     public List<User> findByGender(Connection con, String gender) throws SQLException {
-        return null;
+        String sql="select * from usertable where gender=?";
+        PreparedStatement pstmt= con.prepareStatement(sql);
+        pstmt.setString(1,gender);
+        ResultSet rs= pstmt.executeQuery();
+        User user=null;
+        List<User> list=new ArrayList<User>();
+        if(rs.next()){
+            user=new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthdate(rs.getDate("birthdate"));
+            list.add(user);
+        }
+        return list;
     }
 
     @Override
     public List<User> findByBirthdate(Connection con, Date birthDate) throws SQLException {
-        return null;
+        String sql="select * from usertable where birthdate=?";
+        PreparedStatement pstmt= con.prepareStatement(sql);
+        pstmt.setDate(1, (java.sql.Date) birthDate);
+        ResultSet rs= pstmt.executeQuery();
+        User user=null;
+        List<User> list=new ArrayList<User>();
+        if(rs.next()){
+            user=new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthdate(rs.getDate("birthdate"));
+            list.add(user);
+        }
+        return list;
     }
 
     @Override
     public List<User> findAllUser(Connection con) throws SQLException {
-        return null;
+        String sql="select * from usertable";
+        PreparedStatement pstmt= con.prepareStatement(sql);
+        ResultSet rs= pstmt.executeQuery();
+        User user=null;
+        List<User> list=new ArrayList<User>();
+        if(rs.next()){
+            user=new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthdate(rs.getDate("birthdate"));
+            list.add(user);
+        }
+        return list;
     }
 }
--- src/main/java/com/xuhuan/week3/demo/RegisterServlet.java
+++ src/main/java/com/xuhuan/week3/demo/RegisterServlet.java
@@ -107,4 +107,3 @@ public class RegisterServlet extends HttpServlet {
         }
     }
 }
-
